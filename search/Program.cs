9eceff01060f446d7ad28a930daa1e8d013e9c8f
shempa <shempa@mail.ru>
2019-09-16 18:45:58 +0300

    Добавьте файлы проекта.

---
+++ search/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search
+{
+    class Program
+    {
+        static int res;
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Ведите целочисленное значение х");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество элементов одномерного массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите отсортированный по убыванию массив целых чисел");           
+            int[] mas = new int[n];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Индекс первого элемента массива, строго меньшего X");
+            var res = bsearch(mas, x);
+            Console.WriteLine(res);
+            Console.ReadKey();        
+        }
+
+        static int bsearch(int[] mas, int x )
+        {            
+            for (int i = 0; i<mas.Length;i++)
+            {                
+                if (mas[i] < x)
+                {
+                    res=i;                   
+                    break;                                     
+                }               
+            }
+            return res;
+        }
+    }
+}
